f4aa600ee4b43a5e8ba8e7767f2d24d17e1f7533
Donald Parish <donald.parish@gmail.com>
2010-03-09 23:27:03 -0600

    Enter a directory instead of a list of files to run all *_test.rb files recursivly. 'test' to run everything in 'test' directory, 'test/unit' to run all unit tests, etc.

--- lib/spork/test_framework/test_unit.rb
+++ lib/spork/test_framework/test_unit.rb
@@ -6,6 +6,8 @@ class Spork::TestFramework::TestUnit < Spork::TestFramework
     Object.send(:remove_const, :STDOUT); Object.send(:const_set, :STDOUT, stdout)
     require 'test/unit/autorunner'
     r = Test::Unit::AutoRunner.new(true)
+    only_arg_is_directory = argv.count == 1 and File.directory? File.expand_path(argv[0])
+    argv = Dir[File.join(argv, '**','*_test.rb')] if only_arg_is_directory
     exit(false) unless r.process_args(argv)
     r.run
   rescue
